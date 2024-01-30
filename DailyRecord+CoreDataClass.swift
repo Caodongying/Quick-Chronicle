edd5e821a2b31b2cd0c6f0d693975afa746787ad
Caodongying <jsjhcdy@163.com>
2024-01-30 23:00:31 +0100

    god damn it worked but i didn't know

---
+++ DailyRecord+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DailyRecord+CoreDataClass.swift
+//  Quick Chronicle
+//
+//  Created by 曹冬颖 on 2024-01-30.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class DailyRecord: NSManagedObject {
+
+}
